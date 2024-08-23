a79111f6a2118ab58b51da884fcce030ee4c4029
oktaytekli <oktay.tekli@hotmail.com>
2024-08-23 10:58:07 +0300

    select keyword

---
+++ SelectWork.sql
@@ -0,0 +1,11 @@
+-- Tek satirlik yorum satiri
+/*
+
+*/
+-- select veritabanındaki tablolari ya da view'leri sorgulamaya yarar.
+-- Gnele kullanimi select * from <tablo_adi>
+--Ornekler
+-- productlari listeleyelim
+
+--select * from products;
+
