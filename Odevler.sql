3d71ed2dcb30271937459899c0ba34612395db7d
oktaytekli <oktay.tekli@hotmail.com>
2024-08-26 08:21:58 +0300

    gg

---
+++ Odevler.sql
@@ -0,0 +1,10 @@
+Ulkelerin yaptigi cirolari bulalim.
+Ulkelerin yaptigi ciorlari yillara gore dagitalim.
+
+calisanlarin yaptigi ciro nedir (1997 yili) 
+
+kargo firmalarinin tasidigi siparislerin toplam degeri nedir 
+
+kategorilere gore ciro nedir ?
+
+1997 yilindaki cirolarin aylara gore dagilimi nedir ?
